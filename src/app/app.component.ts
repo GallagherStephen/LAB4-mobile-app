99669cb3b6633dcc971ec454aeadc87104219e44
G00314689 <G00314689@CRBYOD17>
2019-02-12 17:40:16 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab4';
+//////////////////////////////////////////////////////////////////
+  counter:number =  0; //starts count number of clicks to 0
+
+  onButtonClick()     //method to add 1 to each click
+  {
+    this.counter++;
+  }
+//////////////////////////////////////////////////////////////////
+  onButtonClick2()     //method to add 1 to each  double click
+  {
+    this.counter2++;
+  }
+  counter2:number =  0; //starts count number of clicks to 0 for double click
+
+//////////////////////////////////////////////////////////////////////
+  hide:boolean = true;
+  onHide()    
+  {
+    console.log("image clicked")
+    if(this.hide == false)
+    {
+      this.hide = true;
+    }else{
+      this.hide = false;
+    }
+  }
+
+
+
 }
